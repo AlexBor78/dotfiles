b8309dbc54bb2ffacc9d5fde06ea36d55b486511
AlexBor78 <you@example.com>
2026-05-16 17:08:17 +0300

    move /nix/home -> /nix/hm (home-manager)

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
         home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.${username} = import ./nix/home;
+          home-manager.users.${username} = import ./nix/hm;
           home-manager.extraSpecialArgs = { 
             inherit self username dotsroot nixvim tokyonight rofi-theme;
 						hostname = "t480";
@@ -71,7 +71,7 @@
         home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.${username} = import ./nix/home;
+          home-manager.users.${username} = import ./nix/hm;
           home-manager.extraSpecialArgs = { 
             inherit self username dotsroot nixvim tokyonight rofi-theme;
 						hostname = "x13";
