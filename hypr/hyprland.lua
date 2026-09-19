5a058f9e1dc074a88cd6e24da11a7ba2bee1b3b0
AlexBor78 <you@example.com>
2026-09-19 17:25:10 +0300

    fixe animation in sreenshot

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -5,5 +5,5 @@ require(basedir .. "base.lua")
 require(basedir .. "binds.lua")
 require(basedir .. "monitors.lua")
 require(basedir .. "looknfeel.lua")
--- require(basedir .. "ruls.lua")
+require(basedir .. "ruls.lua")
 
